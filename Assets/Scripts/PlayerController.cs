52911910ccc83f351828e2ab6c5bbaac8146e125
westonbattles <weston@battles.org>
2026-04-03 12:12:32 -0700

    single jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public float jumpForce;
+    public Vector3 feetPosition = new Vector3(0f, -0.48f, 0f);
+    public LayerMask groundLayer;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
@@ -15,6 +18,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int extraJumps = 0;
+    private bool shouldJump = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,6 +36,20 @@ public class PlayerController : MonoBehaviour
     {   
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+        if (shouldJump)
+        {
+            // catch frame where the game thinks we are grounded but we've already added jump force
+            if (isGrounded() )
+            {
+                rb.AddForce(Vector3.up * jumpForce);
+                Debug.Log("JUMPED!");
+            }
+
+            shouldJump = false;
+        }
+        
+        
     }
 
     void OnTriggerEnter(Collider other) 
@@ -55,8 +74,18 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void OnJump(InputValue movementValue)
+    {
+        shouldJump = true;
+    }
+
     void SetCountText() 
    {
        countText.text =  "Count: " + count.ToString();
    }
+
+   bool isGrounded()
+    {
+        return Physics.Raycast(transform.position + feetPosition, Vector3.down, 0.1f, groundLayer);
+    }
 }
